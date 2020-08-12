9c9ddcbef396834c2cfd5f04287d411631cecd01
Lucas de Melo Souza <lucasmelosouza8@gmail.com>
2020-08-12 15:55:22 -0300

    Update Seller and custom service exception

---
+++ SalesWebMvc/Services/Exceptions/NotfoundExceptions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SalesWebMvc.Services.Exceptions
+{
+    public class NotfoundExceptions : ApplicationException
+    {
+        public NotfoundExceptions(string message) : base(message)
+        {
+
+        }
+    }
+}
